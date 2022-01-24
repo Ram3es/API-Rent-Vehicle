89baeab3ddebdb992b726eb273687a772cfc0d59
Roman Luzan <romaluzan1@gmail.com>
2022-01-24 22:13:21 +0200

    connected_db created_user_entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,29 @@
+import { User } from './user/user.entity';
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { UserModule } from "./user/user.module";
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    envFilePath: `.${process.env.NODE_ENV}.env`
-  })],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+    }),
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      entities: [User],
+      synchronize: true,
+      autoLoadEntities: true
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/user/user.entity.ts
@@ -0,0 +1,21 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity("users")
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+  @Column({ unique: true, nullable: false })
+  email: string;
+  @Column({ nullable: false })
+  password: string;
+  @Column({default: false})
+  isActive:boolean
+  @Column({nullable: true})
+  firstName:string
+  @Column({nullable: true})
+  lastName: string
+  @CreateDateColumn()
+  createdAt: Date
+  @UpdateDateColumn()
+  updatedAt: Date
+}
